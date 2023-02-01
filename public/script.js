602c639b4957d2b73cbc3cf7ceda30e46b7ae8a0
quentin <clavier.quentin@gmail.com>
2023-02-01 11:45:58 +0100

    validation behavior & complete state layout OK. TODO responsive for mobile

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
-let errorBorderColor = "hsl(0, 100%, 66%)";
-
 let inputList = document.querySelectorAll("input");
 
 function ValidateCreditCardNumber(cardNumber) {
 
-    var visaRegEx = /^[0-9]{16}$/;
-    var isValid = false;
-    if (visaRegEx.test(cardNumber.split(" ").join(""))) {
+    let visaRegEx = /^[0-9]{16}$/;
+    let isValid = false;
+
+    let cardNumberToTest = cardNumber.split(" ").join("");
+    if (visaRegEx.test(cardNumberToTest)) {
       isValid = true;
     }
   
@@ -14,18 +14,35 @@ function ValidateCreditCardNumber(cardNumber) {
        document.getElementById("numberError").textContent = "Wrong format, number only";
        document.getElementById("number").style.borderColor = errorBorderColor;
     }
+}
+
+function displayValuesFromInput(inputName, value){
+  let cardNumber = value.split(" ").join("");
+  if(inputName == "number"){
+    let str = "";
+    for(i = 0; i < cardNumber.length; i++){
+        str += cardNumber.substring(i, i+4) + " ";
+        i = i + 3; 
+    }
+    let span = document.getElementById("numberSpan");
+    span.textContent = str;
+  } else{
+    let span = document.getElementById(inputName + "Span");
+    span.textContent = value.toUpperCase();
   }
+}
 
 inputList.forEach(element => {
     element.addEventListener("input", () => {
-        let span = document.getElementById(element.name + "Span");
-        span.textContent = element.value.toUpperCase();
+      displayValuesFromInput(element.name, element.value)
     });
 });
 
 document.getElementById("btn").addEventListener('click', () => {
+
     let form = document.querySelector("form");
     let formData = new FormData(form);
+    let isFormValidated = true;
 
     for(const [key, value] of formData){
         let valueIsNull = value == "" ? true : false;
@@ -35,9 +52,29 @@ document.getElementById("btn").addEventListener('click', () => {
             } else{
                 document.getElementById(key + "Error").textContent = "Can't be blank";
             }
-            document.getElementById(key).style.borderColor = errorBorderColor;
+            document.getElementById(key).classList.add("errorBorder");
+            isFormValidated = false;
+        } else{
+          document.getElementById(key).classList.remove("errorBorder") ;
+          if(key == "month" || key == "year"){
+            document.getElementById("dateError").textContent = "";
+          } else{
+            document.getElementById(key + "Error").textContent = "";
+          }
         }
     }
+
     ValidateCreditCardNumber(formData.get("number"));
+    if(isFormValidated){
+      for(const [key, value] of formData){
+        displayValuesFromInput(key, value);
+      }
+      form.style.display = "none";
+      document.getElementById("completeStateDiv").style.display = "flex";
+    }
 });
+
+document.getElementById("btnReset").addEventListener("click", () => {
+  location.reload();
+})
 
